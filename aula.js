e00eef8dcadbde204fc4abf98747ea434ccb5aa6
Igor Muniz <62313187+IgorMunizRj40@users.noreply.github.com>
2021-01-12 20:56:24 -0300

    Add files initial

---
+++ aula.js
@@ -0,0 +1,10 @@
+console.log('Galdino')
+console.log('Bem vindo ao curso')
+let idade = 33; //variavel let ou var a mais atual e let 
+console.log(idade); //imprimir na tela a let(variavel) idade
+let nome = "Igor";
+console.log(nome); 
+let sexo = "Masculino";
+console.log(sexo);
+let time = "Flamengo";
+console.log(time);
