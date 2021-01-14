ba448d1fc6b62e8ee4452afc81680839d944c47a
Igor Muniz <igor.galdinomuniz@gmail.com>
2021-01-13 21:07:05 -0300

    Tipos primitivos de referências

---
+++ scripts.js
@@ -0,0 +1,11 @@
+//Tipos primitivos de referencias
+let nome = "Alex"; // string literal
+let idade = 40; //number literal
+let estaAprovado = true; //bolean
+let sobreNome = undefined; //indefinido
+let corSelecionada = null; //usada para redefinir valores
+console.log(nome);
+console.log(idade);
+console.log(estaAprovado);
+console.log(sobreNome);
+console.log(corSelecionada);
