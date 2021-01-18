e06a1eaa516acf594702106028cda0ff52194a81
Igor Muniz <igor.galdinomuniz@gmail.com>
2021-01-18 13:00:05 -0300

    Implementação de funções

--- scripts.js
+++ scripts.js
@@ -1,16 +1,23 @@
-//Arrays
+//Functions
+let corSite ="azul";
+function resetaCor(cor)//quando os parenteses estiver vazio que dizer que esta sem parametros
+{
+    corSite = cor;
+};
 
-let familia = [8,33,33.8];
+console.log(corSite);//imprimi a var corSite
+resetaCor("vermelho");//Chama a funcao e reseta a cor
+console.log(corSite);//imprimi a cor resetada
 
-console.log(familia); //imprimi todos os valores dos indices
-console.log(familia[0]); //imprimi somente o primeiro indice
-console.log(familia.length);
+//Dois parametros
+let corFonte ="azul";
+function resetaCorFonte(cor,tonalidade)//quando os parenteses estiver vazio que dizer que esta sem parametros
+{
+    corFonte = cor + ' ' + tonalidade;//usamos a aspas simples pois na hora de concatenar o que for impresso tera um espacamento padrao
+};
 
-let idadeFamilia = [8,33,true, "Alex"];
-console.log(idadeFamilia); //imprimi todos os valores dos indices
-console.log(idadeFamilia[3]); //imprimi somente o quarto indice
-console.log(idadeFamilia[2]); //imprimi somente o terceiro indice
+console.log(corFonte);//imprimi a var corSite
+resetaCorFonte("verde", "claro");//Chama as funcoes e altera a cor
+console.log(corFonte);//imprimi a cor resetada
 
-let nomeAmigo = ["João",8,"São João de Meriti"];
-console.log(nomeAmigo);
-console.log(nomeAmigo[1]);
+//alteraTamanho tambem e um ex 
