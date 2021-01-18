0df4ceccbc62a37a7d3f5bcee06c77fc114a4e18
Igor Muniz <igor.galdinomuniz@gmail.com>
2021-01-17 21:17:02 -0300

    Arrays

--- scripts.js
+++ scripts.js
@@ -1,17 +1,16 @@
-//Tipos primitivos de referencias
-//Tipagem Dinamica
-let nome = "Alex"; // string literal
-let idade = 40; //number literal
-let estaAprovado = true; //bolean
-let sobreNome = undefined; //indefinido
-let corSelecionada = null; //usada para redefinir valores
+//Arrays
 
-let pessoa = {
-    nome : 'Alex',
-    idade: 40,
-    estaAprovado: true,
-    sobreNome: 'Diniz'
+let familia = [8,33,33.8];
 
-};
+console.log(familia); //imprimi todos os valores dos indices
+console.log(familia[0]); //imprimi somente o primeiro indice
+console.log(familia.length);
 
-console.log(pessoa);
+let idadeFamilia = [8,33,true, "Alex"];
+console.log(idadeFamilia); //imprimi todos os valores dos indices
+console.log(idadeFamilia[3]); //imprimi somente o quarto indice
+console.log(idadeFamilia[2]); //imprimi somente o terceiro indice
+
+let nomeAmigo = ["João",8,"São João de Meriti"];
+console.log(nomeAmigo);
+console.log(nomeAmigo[1]);
